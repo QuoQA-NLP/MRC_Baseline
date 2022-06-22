873cbb0db02f1152cdfe0f86398b51fad71b37f5
snoop2head <young_ahn@yonsei.ac.kr>
2022-06-22 22:48:34 +0900

    update training arguments with gradient checkpointing: 13GB, 450 max length, batch size 32

--- running_train_only.sh
+++ running_train_only.sh
@@ -9,23 +9,19 @@ python train.py \
 --model_name RobertaForV2QuestionAnswering \
 --max_length 450 \
 --stride 128 \
---logging_strategy steps \
---logging_steps 100 \
---evaluation_strategy steps \
 --save_strategy steps \
 --save_steps 250 \
 --overwrite_output_dir \
 --save_total_limit 10 \
 --output_dir ./exps \
 --logging_dir ./logs \
---learning_rate 2e-5 \
+--learning_rate 1e-5 \
 --num_train_epochs 5 \
 --max_steps 3000 \
---per_device_train_batch_size 4 \
---per_device_eval_batch_size 4 \
---gradient_accumulation_steps 8 \
+--per_device_train_batch_size 32 \
 --warmup_ratio 0.05 \
---weight_decay 1e-2
+--weight_decay 1e-2 \
+--gradient_checkpointing
 
 python inference.py \
 --do_predict \
@@ -37,5 +33,4 @@ python inference.py \
 --output_dir results \
 --overwrite_output_dir \
 --per_device_eval_batch_size 16 \
---gradient_accumulation_steps 1 \
 --file_name roberta_base_EP:5_BS:16_WR:0.05_WD:1e-3.csv
