253ec533c998756a615c7b27e125e8e9d590be5b
snoop2head <young_ahn@yonsei.ac.kr>
2022-06-23 10:26:23 +0900

    reproduction with 32

--- running_train_only.sh
+++ running_train_only.sh
@@ -10,20 +10,22 @@ python train.py \
 --max_length 460 \
 --stride 128 \
 --save_strategy steps \
---save_steps 250 \
+--save_steps 125 \
 --overwrite_output_dir \
 --save_total_limit 10 \
 --output_dir ./exps \
 --logging_dir ./logs \
---learning_rate 1e-5 \
---num_train_epochs 5 \
---max_steps 3000 \
+--learning_rate 2e-5 \
+--weight_decay 2e-2 \
 --per_device_train_batch_size 32 \
---gradient_accumulation_steps 1 \
---warmup_ratio 0.10 \
---weight_decay 1e-2 \
+--per_device_eval_batch_size 32 \
+--gradient_accumulation_steps 2 \
 --gradient_checkpointing \
+--warmup_ratio 0.10 \
+--max_steps 1500 \
 --seed 42
+
+
 
 # python inference.py \
 # --do_predict \
